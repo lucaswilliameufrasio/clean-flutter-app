ae1020839080e1dab419df67fd170450830006c4
Lucas William <lucaswilliameufrasio@gmail.com>
2020-09-21 00:46:11 -0300

    refactor: move api details from domain entity to data model

--- lib/domain/entities/account_entity.dart
+++ lib/domain/entities/account_entity.dart
@@ -1,8 +1,5 @@
 class AccountEntity {
-    final String token;
+  final String token;
 
   AccountEntity(this.token);
-  
-  factory AccountEntity.fromJson(Map json) =>
-    AccountEntity(json['accessToken']);
 }
