33e2917b27fa87976f1dee952ad7b1d2fecd5c4a
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-04 20:22:29 -0300

    feat: ensure GetxSignUpPresenter emits isFormValid true if all fields are valid

--- lib/presentation/presenters/getx_signup_presenter.dart
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -7,6 +7,10 @@ import '../protocols/protocols.dart';
 class GetxSignUpPresenter extends GetxController {
   final Validation validation;
 
+  String _name;
+  String _email;
+  String _password;
+  String _passwordConfirmation;
   var _emailError = Rx<UIError>();
   var _nameError = Rx<UIError>();
   var _passwordError = Rx<UIError>();
@@ -16,7 +20,8 @@ class GetxSignUpPresenter extends GetxController {
   Stream<UIError> get emailErrorStream => _emailError.stream;
   Stream<UIError> get nameErrorStream => _nameError.stream;
   Stream<UIError> get passwordErrorStream => _passwordError.stream;
-  Stream<UIError> get passwordConfirmationErrorStream => _passwordConfirmationError.stream;
+  Stream<UIError> get passwordConfirmationErrorStream =>
+      _passwordConfirmationError.stream;
   Stream<bool> get isFormValidStream => _isFormValid.stream;
 
   GetxSignUpPresenter({
@@ -24,27 +29,40 @@ class GetxSignUpPresenter extends GetxController {
   });
 
   void validateEmail(String email) {
+    _email = email;
     _emailError.value = _validateField(field: 'email', value: email);
     _validateForm();
   }
 
   void validateName(String name) {
+    _name = name;
     _nameError.value = _validateField(field: 'name', value: name);
     _validateForm();
   }
 
   void validatePassword(String password) {
+    _password = password;
     _passwordError.value = _validateField(field: 'password', value: password);
     _validateForm();
   }
 
   void validatePasswordConfirmation(String passwordConfirmation) {
-    _passwordConfirmationError.value = _validateField(field: 'passwordConfirmation', value: passwordConfirmation);
+    _passwordConfirmation = passwordConfirmation;
+    _passwordConfirmationError.value = _validateField(
+        field: 'passwordConfirmation', value: passwordConfirmation);
     _validateForm();
   }
 
   void _validateForm() {
-    _isFormValid.value = false;
+    _isFormValid.value = _nameError.value == null &&
+        _emailError.value == null &&
+        _passwordError.value == null &&
+        _passwordConfirmationError.value == null &&
+        _passwordError.value == null &&
+        _name != null &&
+        _email != null &&
+        _password != null &&
+        _passwordConfirmation != null;
   }
 
   UIError _validateField({String field, String value}) {
