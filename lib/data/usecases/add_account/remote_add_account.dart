bc0d62e8fa6c2c030346868d2ff145a98970c3e6
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-04 03:16:02 -0300

    feat: ensure RemoteAddAccount returns an Account on 200

--- lib/data/usecases/add_account/remote_add_account.dart
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -1,20 +1,24 @@
 import 'package:meta/meta.dart';
 
 import '../../../domain/usecases/usecases.dart';
+import '../../../domain/entities/entities.dart';
 import '../../../domain/helpers/helpers.dart';
 
 import '../../http/http.dart';
+import '../../models/models.dart';
 
-class RemoteAddAccount {
+class RemoteAddAccount implements AddAccount {
   final HttpClient httpClient;
   final String url;
 
   RemoteAddAccount({@required this.httpClient, @required this.url});
 
-  Future<void> add(AddAccountParams params) async {
+  Future<AccountEntity> add(AddAccountParams params) async {
     final body = RemoteAddAccountParams.fromDomain(params).toJson();
     try {
-      await this.httpClient.request(url: url, method: 'post', body: body);
+      final httpResponse =
+          await this.httpClient.request(url: url, method: 'post', body: body);
+      return RemoteAccountModel.fromJson(httpResponse).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.forbidden
           ? DomainError.emailInUse
