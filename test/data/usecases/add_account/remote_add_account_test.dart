af1dfbbde7eecbb9f7b3c854ab8323fdbda724ae
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-04 03:07:27 -0300

    test: ensure RemoteAddAccount throws UnexpectedError on 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -59,4 +59,13 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
