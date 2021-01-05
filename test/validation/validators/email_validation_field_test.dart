70d11c26af62aa3fb13fd2c9f4e671c1aac62716
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-05 03:51:53 -0300

    refactor: change Validation interface to receive an object instead of a String

--- test/validation/validators/email_validation_field_test.dart
+++ test/validation/validators/email_validation_field_test.dart
@@ -12,18 +12,18 @@ void main() {
   });
 
   test('Should return null if email is empty', () {
-    expect(sut.validate(''), null);
+    expect(sut.validate({'any_field': ''}), null);
   });
 
   test('Should return null if email is null', () {
-    expect(sut.validate(null), null);
+    expect(sut.validate({'any_field': null}), null);
   });
 
   test('Should return null if email is valid', () {
-    expect(sut.validate('lucaswilliameufrasio@gmail.com'), null);
+    expect(sut.validate({'any_field': 'lucaswilliameufrasio@gmail.com'}), null);
   });
 
   test('Should return error if email is invalid', () {
-    expect(sut.validate('lucaswilliameufrasio'), ValidationError.invalidField);
+    expect(sut.validate({'any_field': 'lucaswilliameufrasio'}), ValidationError.invalidField);
   });
 }
