c822110905938593f4707eb93c3b87a231b08de8
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-04 21:12:08 -0300

    feat: ensure GetxSignUpPresenter navigates to Surveys Page on AddAccount success

--- lib/presentation/presenters/getx_signup_presenter.dart
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -24,6 +24,7 @@ class GetxSignUpPresenter extends GetxController {
   var _mainError = Rx<UIError>();
   var _isFormValid = false.obs;
   var _isLoading = false.obs;
+  var _navigateTo = RxString();
 
   Stream<UIError> get emailErrorStream => _emailError.stream;
   Stream<UIError> get nameErrorStream => _nameError.stream;
@@ -33,6 +34,7 @@ class GetxSignUpPresenter extends GetxController {
   Stream<UIError> get mainErrorStream => _mainError.stream;
   Stream<bool> get isFormValidStream => _isFormValid.stream;
   Stream<bool> get isLoadingStream => _isLoading.stream;
+  Stream<String> get navigateToStream => _navigateTo.stream;
 
   GetxSignUpPresenter({
     @required this.validation,
@@ -99,6 +101,7 @@ class GetxSignUpPresenter extends GetxController {
         passwordConfirmation: _passwordConfirmation,
       ));
       await saveCurrentAccount.save(account);
+      _navigateTo.value = '/surveys';
     } on DomainError catch (error) {
       switch (error) {
         case DomainError.emailInUse:
