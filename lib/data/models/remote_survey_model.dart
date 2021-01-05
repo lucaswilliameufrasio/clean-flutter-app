a1ecaf9e8150c11f5bd0f11337b43a70c62d0c1e
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-05 05:31:21 -0300

    feat: ensure RemoteLoadSurvey throws UnexpectedError if HttpClient returns 200 with invalid data

--- lib/data/models/remote_survey_model.dart
+++ lib/data/models/remote_survey_model.dart
@@ -1,4 +1,8 @@
+import 'package:meta/meta.dart';
+
 import '../../domain/entities/entities.dart';
+
+import '../../data/http/http.dart';
 
 class RemoteSurveyModel {
   final String id;
@@ -7,13 +11,19 @@ class RemoteSurveyModel {
   final bool didAnswer;
 
   RemoteSurveyModel({
-    this.id,
-    this.question,
-    this.date,
-    this.didAnswer,
+    @required this.id,
+    @required this.question,
+    @required this.date,
+    @required this.didAnswer,
   });
 
   factory RemoteSurveyModel.fromJson(Map json) {
+    if (!json.keys
+        .toSet()
+        .containsAll(['id', 'question', 'date', 'didAnswer'])) {
+      throw HttpError.invalidData;
+    }
+
     return RemoteSurveyModel(
       id: json['id'],
       question: json['question'],
