fe30397a99cd7f8c913cca9fac30c7ee2f8880be
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-05 08:28:25 -0300

    test: ensure SurveysPage calls loadData on reload button tap

--- test/ui/pages/surveys_pages_test.dart
+++ test/ui/pages/surveys_pages_test.dart
@@ -44,9 +44,11 @@ void main() {
   }
 
   List<SurveyViewModel> makeSurveys() => [
-    SurveyViewModel(id: '1', question: 'Question 1', date: 'Date 1', didAnswer: true),
-    SurveyViewModel(id: '2', question: 'Question 2', date: 'Date 2', didAnswer: false),
-  ];
+        SurveyViewModel(
+            id: '1', question: 'Question 1', date: 'Date 1', didAnswer: true),
+        SurveyViewModel(
+            id: '2', question: 'Question 2', date: 'Date 2', didAnswer: false),
+      ];
 
   tearDown(() {
     closeStreams();
@@ -110,5 +112,16 @@ void main() {
     expect(find.text('Question 2'), findsWidgets);
     expect(find.text('Date 1'), findsWidgets);
     expect(find.text('Date 2'), findsWidgets);
+  });
+
+  testWidgets('Should call LoadSurveys on reload button tap',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    loadSurveysController.addError(UIError.unexpected.description);
+    await tester.pump();
+    await tester.tap(find.text('Recarregar'));
+
+    verify(presenter.loadData()).called(2);
   });
 }
