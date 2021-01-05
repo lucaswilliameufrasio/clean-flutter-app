cdc90ed03a736e1f79d85bd0c5312be969589dde
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-04 21:21:08 -0300

    test: ensure Login navigates to signup on link click

--- test/presentation/presenters/getx_login_presenter_test.dart
+++ test/presentation/presenters/getx_login_presenter_test.dart
@@ -223,4 +223,11 @@ void main() {
 
     await sut.auth();
   });
+
+  test('Should goToSignUp page on link click', () async {
+    sut.navigateToStream
+        .listen(expectAsync1((page) => expect(page, '/signup')));
+
+    sut.goToSignUp();
+  });
 }
