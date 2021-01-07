735416c27b023949af137353e01ec8f40dfcf179
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-07 00:45:49 -0300

    test: ensure LocalLoadSurveys throws UnexpectedError if cache throws

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -13,8 +13,8 @@ class LocalLoadSurveys {
   LocalLoadSurveys({@required this.fetchCacheStorage});
 
   Future<List<SurveyEntity>> load() async {
-    final data = await fetchCacheStorage.fetch('surveys');
     try {
+      final data = await fetchCacheStorage.fetch('surveys');
       if (data?.isEmpty != false) {
         throw Exception();
       }
@@ -54,10 +54,14 @@ void main() {
         },
       ];
 
+  PostExpectation mockFetchCall() => when(fetchCacheStorage.fetch(any));
+
   void mockFetch(List<Map> list) {
     data = list;
-    when(fetchCacheStorage.fetch(any)).thenAnswer((_) async => data);
+    mockFetchCall().thenAnswer((_) async => data);
   }
+
+  void mockFetchError() => mockFetchCall().thenThrow(Exception());
 
   setUp(() {
     fetchCacheStorage = FetchCacheStorageSpy();
@@ -125,6 +129,13 @@ void main() {
         'didAnswer': 'false',
       }
     ]);
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if FetchCacheStorage throws', () async {
+    mockFetchError();
     final future = sut.load();
 
     expect(future, throwsA(DomainError.unexpected));
