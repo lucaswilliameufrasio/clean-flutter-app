22a148707e216b067e9989d3ff9a7dc4e932edee
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-08 11:56:27 -0300

    test: ensure SurveyResultPage loads data correctly

--- test/ui/pages/survey_result_page_test.dart
+++ test/ui/pages/survey_result_page_test.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
 import 'package:get/get.dart';
 
+import 'package:ForDev/ui/pages/survey_result/components/components.dart';
 import 'package:ForDev/ui/helpers/helpers.dart';
 import 'package:ForDev/ui/pages/pages.dart';
 
@@ -14,11 +15,11 @@ class SurveyResultPresenterSpy extends Mock implements SurveyResultPresenter {}
 void main() {
   SurveyResultPresenterSpy presenter;
   StreamController<bool> isLoadingController;
-  StreamController<dynamic> surveyResultController;
+  StreamController<SurveyResultViewModel> surveyResultController;
 
   void initStreams() {
     isLoadingController = StreamController<bool>();
-    surveyResultController = StreamController<dynamic>();
+    surveyResultController = StreamController<SurveyResultViewModel>();
   }
 
   void mockStreams() {
@@ -50,6 +51,24 @@ void main() {
       await tester.pumpWidget(surveysPage);
     });
   }
+
+  SurveyResultViewModel makeSurveyResult() => SurveyResultViewModel(
+        surveyId: 'Any id',
+        question: 'Question',
+        answers: [
+          SurveyAnswerViewModel(
+            image: 'Image 0',
+            answer: 'Answer 0',
+            percent: '60%',
+            isCurrentAnswer: true,
+          ),
+          SurveyAnswerViewModel(
+            answer: 'Answer 1',
+            percent: '40%',
+            isCurrentAnswer: false,
+          ),
+        ],
+      );
 
   tearDown(() {
     closeStreams();
@@ -96,6 +115,7 @@ void main() {
     expect(find.text('Algo de errado aconteceu. Tente novamente em breve.'),
         findsOneWidget);
     expect(find.text('Recarregar'), findsOneWidget);
+    expect(find.text('Question'), findsNothing);
   });
 
   testWidgets('Should call LoadSurveyResult on reload button tap',
@@ -107,5 +127,29 @@ void main() {
     await tester.tap(find.text('Recarregar'));
 
     verify(presenter.loadData()).called(2);
+  });
+
+  testWidgets('Should present valid data if surveyResultStream succeeds',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    surveyResultController.add(makeSurveyResult());
+    await provideMockedNetworkImages(() async {
+      await tester.pump();
+    });
+
+    expect(find.text('Algo de errado aconteceu. Tente novamente em breve.'),
+        findsNothing);
+    expect(find.text('Recarregar'), findsNothing);
+    expect(find.text('Question'), findsOneWidget);
+    expect(find.text('Answer 0'), findsOneWidget);
+    expect(find.text('Answer 1'), findsOneWidget);
+    expect(find.text('60%'), findsOneWidget);
+    expect(find.text('40%'), findsOneWidget);
+    expect(find.byType(ActiveIcon), findsOneWidget);
+    expect(find.byType(DisabledIcon), findsOneWidget);
+    final image =
+        tester.widget<Image>(find.byType(Image)).image as NetworkImage;
+    expect(image.url, 'Image 0');
   });
 }
