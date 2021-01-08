10b8015ba2a140600517953241227e5b55a4a67f
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-08 15:28:50 -0300

    feat: ensure AuthorizeHttpClientDecorator delete cache on ForbiddenError

--- lib/main/decorators/authorize_http_client_decorator.dart
+++ lib/main/decorators/authorize_http_client_decorator.dart
@@ -30,9 +30,10 @@ class AuthorizeHttpClientDecorator implements HttpClient {
         body: body,
         headers: authorizedHeaders,
       );
-    } on HttpError {
-      rethrow;
     } catch (error) {
+      if (error is HttpError && error != HttpError.forbidden) {
+        rethrow;
+      }
       await deleteSecureCacheStorage.deleteSecure('token');
       throw HttpError.forbidden;
     }
