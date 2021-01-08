94f64d102ceaa56757cc96f94f21d0a02e4118c4
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-08 11:08:30 -0300

    refactor: move ReloadScreen Component to its own file

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -33,26 +33,8 @@ class SurveysPage extends StatelessWidget {
               stream: presenter.surveysStream,
               builder: (context, snapshot) {
                 if (snapshot.hasError) {
-                  return Padding(
-                    padding: EdgeInsets.all(40),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          snapshot.error,
-                          style: TextStyle(
-                            fontSize: 16,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(height: 10),
-                        RaisedButton(
-                          child: Text(R.strings.reload),
-                          onPressed: presenter.loadData,
-                        )
-                      ],
-                    ),
-                  );
+                  return ReloadScreen(
+                      error: snapshot.error, reload: presenter.loadData);
                 }
 
                 if (snapshot.hasData) {
