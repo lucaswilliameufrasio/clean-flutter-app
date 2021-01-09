266ecbf2780533a65d712d3183f57d9d5320208f
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-09 20:58:01 -0300

    feat: add LocalLoadSurveyResult loadBySurvey with tests

---
+++ lib/data/models/local_survey_answer_model.dart
@@ -0,0 +1,38 @@
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/entities.dart';
+
+class LocalSurveyAnswerModel {
+  final String image;
+  final String answer;
+  final int percent;
+  final bool isCurrentAnswer;
+
+  LocalSurveyAnswerModel({
+    this.image,
+    @required this.answer,
+    @required this.percent,
+    @required this.isCurrentAnswer,
+  });
+
+  factory LocalSurveyAnswerModel.fromJson(Map json) {
+    if (!json.keys
+        .toSet()
+        .containsAll(['answer', 'percent', 'isCurrentAnswer'])) {
+      throw Exception();
+    }
+    return LocalSurveyAnswerModel(
+      image: json['image'],
+      answer: json['answer'],
+      isCurrentAnswer: json['isCurrentAnswer'].toLowerCase() == 'true',
+      percent: int.parse(json['percent']),
+    );
+  }
+
+  SurveyAnswerEntity toEntity() => SurveyAnswerEntity(
+        image: image,
+        answer: answer,
+        percent: percent,
+        isCurrentAnswer: isCurrentAnswer,
+      );
+}
