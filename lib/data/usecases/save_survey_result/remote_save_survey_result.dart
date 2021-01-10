ce41abd278584e3e66d85e884633f7125ede2db0
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-10 00:30:51 -0300

    feat: ensure RemoteSaveSurveyResult returns SurveyResult on success

--- lib/data/usecases/save_survey_result/remote_save_survey_result.dart
+++ lib/data/usecases/save_survey_result/remote_save_survey_result.dart
@@ -1,18 +1,22 @@
 import 'package:meta/meta.dart';
 
 import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../../domain/entities/entities.dart';
 import '../../http/http.dart';
+import '../../models/models.dart';
 
-class RemoteSaveSurveyResult {
+class RemoteSaveSurveyResult implements SaveSurveyResult {
   final String url;
   final HttpClient httpClient;
 
   RemoteSaveSurveyResult({@required this.url, @required this.httpClient});
 
-  Future<void> save({@required String answer}) async {
+  Future<SurveyResultEntity> save({@required String answer}) async {
     try {
-      await httpClient
+      final json = await httpClient
           .request(url: url, method: 'put', body: {'answer': answer});
+      return RemoteSurveyResultModel.fromJson(json).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.forbidden
           ? DomainError.accessDenied
