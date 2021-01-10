b1c9af2ac058dcd57755ce66607828744776b845
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-10 01:15:30 -0300

    refactor: remove duplication

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
 import 'package:meta/meta.dart';
 
-import '../../ui/pages/pages.dart';
-import '../../ui/helpers/helpers.dart';
-
+import '../../domain/entities/entities.dart';
 import '../../domain/usecases/usecases.dart';
 import '../../domain/helpers/helpers.dart';
+
+import '../../ui/pages/pages.dart';
+import '../../ui/helpers/helpers.dart';
 
 import '../mixins/mixins.dart';
 
@@ -27,38 +28,17 @@ class GetxSurveyResultPresenter extends GetxController
   });
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-      final surveyResult =
-          await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers
-            .map((answer) => SurveyAnswerViewModel(
-                  image: answer.image,
-                  answer: answer.answer,
-                  percent: '${answer.percent}%',
-                  isCurrentAnswer: answer.isCurrentAnswer,
-                ))
-            .toList(),
-      );
-    } on DomainError catch (error) {
-      if (error == DomainError.accessDenied) {
-        isSessionExpired = true;
-      } else {
-        _surveyResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
   }
 
   Future<void> save({@required String answer}) async {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async {
     try {
       isLoading = true;
-      final surveyResult = await saveSurveyResult.save(answer: answer);
-
+      final surveyResult = await action();
       _surveyResult.value = SurveyResultViewModel(
         surveyId: surveyResult.surveyId,
         question: surveyResult.question,
