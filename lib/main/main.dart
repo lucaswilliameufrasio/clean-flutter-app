4389e9a0e86fa08e65a78c8085d55e41e51a2a5b
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-10 03:29:05 -0300

    feat: ensure SurveysPage reload data when page appears

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -13,11 +13,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    final routeObserver = Get.put<RouteObserver>(RouteObserver<PageRoute>());
 
     return GetMaterialApp(
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       theme: makeAppTheme(),
+      navigatorObservers: [routeObserver],
       initialRoute: '/',
       getPages: [
         GetPage(name: '/', page: makeSplashPage, transition: Transition.fade),
@@ -32,8 +34,9 @@ class App extends StatelessWidget {
             page: makeSurveysPage,
             transition: Transition.fadeIn),
         GetPage(
-            name: '/survey_result/:survey_id',
-            page: makeSurveyResultPage,),
+          name: '/survey_result/:survey_id',
+          page: makeSurveyResultPage,
+        ),
       ],
     );
   }
--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:get/get.dart';
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
@@ -7,44 +8,62 @@ import '../../mixins/mixins.dart';
 import 'components/components.dart';
 import 'surveys.dart';
 
-class SurveysPage extends StatelessWidget
-    with LoadingManager, NavigationManager, SessionManager {
+class SurveysPage extends StatefulWidget {
   final SurveysPresenter presenter;
 
   const SurveysPage(this.presenter);
 
   @override
+  _SurveysPageState createState() => _SurveysPageState();
+}
+
+class _SurveysPageState extends State<SurveysPage>
+    with LoadingManager, NavigationManager, SessionManager, RouteAware {
+  @override
   Widget build(BuildContext context) {
+    Get.find<RouteObserver>().subscribe(this, ModalRoute.of(context));
     return Scaffold(
       appBar: AppBar(
         title: Text(R.strings.surveys),
       ),
       body: Builder(
         builder: (context) {
-          handleLoading(context, presenter.isLoadingStream);
-          handleSessionExpired(presenter.isSessionExpiredStream);
-          handleNavigation(presenter.navigateToStream);
-          presenter.loadData();
+          handleLoading(context, widget.presenter.isLoadingStream);
+          handleSessionExpired(widget.presenter.isSessionExpiredStream);
+          handleNavigation(widget.presenter.navigateToStream);
+          widget.presenter.loadData();
 
           return StreamBuilder<List<SurveyViewModel>>(
-              stream: presenter.surveysStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return ReloadScreen(
-                      error: snapshot.error, reload: presenter.loadData);
-                }
+            stream: widget.presenter.surveysStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return ReloadScreen(
+                    error: snapshot.error, reload: widget.presenter.loadData);
+              }
 
-                if (snapshot.hasData) {
-                  return Provider(
-                    create: (_) => presenter,
-                    child: SurveyItems(snapshot.data),
-                  );
-                }
+              if (snapshot.hasData) {
+                return Provider(
+                  create: (_) => widget.presenter,
+                  child: SurveyItems(snapshot.data),
+                );
+              }
 
-                return SizedBox(height: 0);
-              });
+              return SizedBox(height: 0);
+            },
+          );
         },
       ),
     );
+  }
+
+  @override
+  void didPopNext() {
+    widget.presenter.loadData();
+  }
+
+  @override
+  void dispose() {
+    Get.find<RouteObserver>().unsubscribe(this);
+    super.dispose();
   }
 }
